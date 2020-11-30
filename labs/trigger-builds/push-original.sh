4701af83e2a0986832e19df4e480d2e1afc86b93
Student User <student@workstation.lab.example.com>
2020-11-29 21:21:52 -0500

    trigger-builds

---
+++ labs/trigger-builds/push-original.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /usr/local/etc/ocp4.config
+cd /home/student/DO288/labs/trigger-builds
+VERSION="original"
+skopeo copy docker-archive:php-70-rhel7-${VERSION}.tar.gz docker://quay.io/${RHT_OCP4_QUAY_USER}/php-70-rhel7:latest
